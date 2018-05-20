5da249533c111ab26e1b3ff0ce2cf99f5b2fe492
kalebjordan <kalebjordandev@gmail.com>
2018-05-20 15:48:02 -0400

    Added TxIn, Convert to usto, and calculate hash for Tx

--- dollar.js
+++ dollar.js
@@ -51,6 +51,14 @@ var makePublicAddress = (privateKey, publicKey) => {
                    .digest('hex');
   return hash
 }
+
+var calculateHashForTX = (signature, address, amount) => {
+  const secret = signature.toString();
+  const hash = crypto.createHmac('sha256', secret)
+                   .update([address, amount].toString())
+                   .digest('hex');
+  return hash
+}
 var testAccount = new Account('maker', 'kalebjordanwilson@gmail.com', 'fakekey', 'api020393');
 var testPrivateKey = makePrivateKey(testAccount)
 var testPublicKey = makePublicKey(testPrivateKey)
@@ -61,12 +69,29 @@ console.log('\ntestPublicKey \n',testPublicKey);
 
 console.log('\ntestPublicAddress \n',testPublicAddress);
 
-var convertToTXO = (TxIn) => {
 
-}
 
 class TxIn {
   constructor(spent, address, amount, publicKey) {
-
+    this.spent = spent
+    this.address = address
+    this.amount = amount
+    this.signature = publicKey
+    this.hash = calculateHashForTX(publicKey, address, amount)
   }
 }
+
+class USTXO {
+  constructor(spent, privateKey) {
+    this.spent = spent
+    this.address = spent.address
+    this.amount = spent.amount
+    this.hash = calculateHashForTX(privateKey, `address`, `amount`)
+  }
+}
+
+var testTxIn = new TxIn('', testPublicAddress, 20, testPublicKey)
+console.log('\ntestTxIn\n', testTxIn);
+
+var testUSTXO = new USTXO(testTxIn, testPrivateKey)
+console.log('\ntestUSTXO\n', testUSTXO);
